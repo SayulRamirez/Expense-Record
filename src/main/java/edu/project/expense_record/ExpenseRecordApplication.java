5f4e836fc1433ecb510daeb97a8bf2b97a9d5bf5
Sayul <sayulramirez@gmail.com>
2024-04-07 13:57:48 -0600

    Creado el proyecto para el registro de gastos

---
+++ src/main/java/edu/project/expense_record/ExpenseRecordApplication.java
@@ -0,0 +1,13 @@
+package edu.project.expense_record;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseRecordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseRecordApplication.class, args);
+    }
+
+}
