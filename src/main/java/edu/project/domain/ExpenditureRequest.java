5528ff6350f92e1dda391e9d92059b2f9e5bf571
Sayul <sayulramirez@gmail.com>
2024-04-07 20:43:34 -0600

    Añadida la etiqueta de la @ValidDateFormat al campo de date para poder validarlo

--- src/main/java/edu/project/domain/ExpenditureRequest.java
+++ src/main/java/edu/project/domain/ExpenditureRequest.java
@@ -1,29 +1,30 @@
 package edu.project.domain;
 
+import edu.project.validations.ValidDateFormat;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
-import org.springframework.format.annotation.DateTimeFormat;
 
-import java.time.LocalDate;
 
 public record ExpenditureRequest(
 
         @Size(min = 5, max = 50, message = "Only between 5 and 50 characters")
-        @NotBlank(message = "The field cannot be null o blank")
+        @NotBlank(message = "The field cannot be null or blank")
         String title,
 
         @Size(min = 5, max = 100, message = "Only between 5 and 10 characters")
-        @NotBlank(message = "The field cannot be null o blank")
+        @NotBlank(message = "The field cannot be null or blank")
         String description,
 
         @Positive(message = "Must be greater than 0")
+        @NotNull(message = "The field cannot be null")
         Double amount,
 
-        @DateTimeFormat(pattern = "^\\d{4}-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])$")
-        LocalDate date,
+        @ValidDateFormat
+        String date,
 
+        @NotBlank(message = "The field cannot be null or blank")
         String category
 ) {
 }
