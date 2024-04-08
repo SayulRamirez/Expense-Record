0d19bf7696eb6b449284ae5719fa3331666b3a3b
Sayul <sayulramirez@gmail.com>
2024-04-07 23:52:43 -0600

    Agregado el método edit para editar los gastos y tambien remplazada la clase CategoryNotFound -> por EntityNotFoundException

--- src/main/java/edu/project/services/ExpenditureService.java
+++ src/main/java/edu/project/services/ExpenditureService.java
@@ -2,9 +2,12 @@ package edu.project.services;
 
 import edu.project.domain.ExpenditureRequest;
 import edu.project.domain.ExpenditureResponse;
+import edu.project.domain.ExpenditureUpdate;
 
 import java.time.LocalDate;
 
 public interface ExpenditureService {
     ExpenditureResponse register(ExpenditureRequest request, LocalDate date);
+
+    ExpenditureResponse edit(ExpenditureUpdate request, LocalDate date);
 }
--- src/main/java/edu/project/services/ExpenditureServiceImpl.java
+++ src/main/java/edu/project/services/ExpenditureServiceImpl.java
@@ -2,11 +2,12 @@ package edu.project.services;
 
 import edu.project.domain.ExpenditureRequest;
 import edu.project.domain.ExpenditureResponse;
+import edu.project.domain.ExpenditureUpdate;
 import edu.project.entities.CategoryEntity;
 import edu.project.entities.CategoryRepository;
 import edu.project.entities.ExpenditureEntity;
 import edu.project.entities.ExpenditureRepository;
-import edu.project.exceptions.CategoryNotFound;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -22,7 +23,7 @@ public class ExpenditureServiceImpl implements ExpenditureService{
     @Override
     public ExpenditureResponse register(ExpenditureRequest request, LocalDate date) {
 
-        CategoryEntity category = categoryRepository.findCategoryEntityByCategory(request.category()).orElseThrow(() -> new CategoryNotFound("Category not found"));
+        CategoryEntity category = categoryRepository.findCategoryEntityByCategory(request.category()).orElseThrow(() -> new EntityNotFoundException("Category not found whit: " + request.category()));
 
         ExpenditureEntity expenditure = ExpenditureEntity.builder()
                 .title(request.title())
@@ -31,6 +32,32 @@ public class ExpenditureServiceImpl implements ExpenditureService{
                 .date(date)
                 .category(category)
                 .build();
+
+        expenditureRepository.save(expenditure);
+
+        return new ExpenditureResponse(
+                expenditure.getTitle(),
+                expenditure.getDescription(),
+                expenditure.getAmount(),
+                expenditure.getDate(),
+                expenditure.getCategory().getCategory());
+    }
+
+    @Override
+    public ExpenditureResponse edit(ExpenditureUpdate request, LocalDate date) {
+
+        ExpenditureEntity expenditure = expenditureRepository.findById(request.id()).orElseThrow(() -> new EntityNotFoundException("The expenditure not found whit id: " + request.id()));
+
+        expenditure.setTitle(request.title());
+        expenditure.setDescription(request.description());
+        expenditure.setAmount(request.amount());
+        expenditure.setDate(date);
+
+        if (!expenditure.getCategory().getCategory().equals(request.category())) {
+
+            CategoryEntity category = categoryRepository.findCategoryEntityByCategory(request.category()).orElseThrow(() -> new EntityNotFoundException("The category not found whit: " + request.category()));
+            expenditure.setCategory(category);
+        }
 
         expenditureRepository.save(expenditure);
 
