408b200b94830ab11e11c057b05ad46d20a84dd5
Sayul <sayulramirez@gmail.com>
2024-04-07 23:55:40 -0600

    Añadido el enpoint put para la edición de los gastos

--- src/main/java/edu/project/controller/ExpenditureController.java
+++ src/main/java/edu/project/controller/ExpenditureController.java
@@ -2,14 +2,13 @@ package edu.project.controller;
 
 import edu.project.domain.ExpenditureRequest;
 import edu.project.domain.ExpenditureResponse;
+import edu.project.domain.ExpenditureUpdate;
 import edu.project.services.ExpenditureService;
 import edu.project.services.ExpenditureServiceImpl;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
@@ -23,12 +22,24 @@ public class ExpenditureController {
         this.expenditureService = expenditureService;
     }
 
+    @Transactional
     @PostMapping
     public ResponseEntity<ExpenditureResponse> registerExpenditure(@Valid @RequestBody ExpenditureRequest request) {
 
         LocalDate date = LocalDate.parse(request.date());
 
         ExpenditureResponse response = expenditureService.register(request, date);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @Transactional
+    @PutMapping
+    public ResponseEntity<ExpenditureResponse> editExpenditure(@Valid @RequestBody ExpenditureUpdate request) {
+
+        LocalDate date = LocalDate.parse(request.date());
+
+        ExpenditureResponse response = expenditureService.edit(request, date);
 
         return ResponseEntity.ok(response);
     }
