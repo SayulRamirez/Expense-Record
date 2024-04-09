2ada4c25644f491b8f0ee306b9b8068a0c0fd3ab
Sayul <sayulramirez@gmail.com>
2024-04-09 17:53:43 -0600

    Nuevo método findAllByDate, para buscar los gastos por una feccha en especifico

--- src/main/java/edu/project/entities/ExpenditureRepository.java
+++ src/main/java/edu/project/entities/ExpenditureRepository.java
@@ -1,8 +1,14 @@
 package edu.project.entities;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+
 @Repository
 public interface ExpenditureRepository extends JpaRepository<ExpenditureEntity, Long> {
+
+    Page<ExpenditureEntity> findAllByDate(LocalDate date, Pageable pageable);
 }
